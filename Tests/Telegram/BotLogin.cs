03196d275c9fc5d338519f47d7a44c839991c52d
Arkady Sheglov <a.sheglov@securitycode.ru>
2022-03-31 18:59:03 +0300

    Initial

---
+++ Tests/Telegram/BotLogin.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Text.Json;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using Telegram.Bot;
+
+namespace Tests.Telegram
+{
+    public class BotLogin
+    {
+        [Test]
+        public void Test()
+        {
+            var content = File.ReadAllText("./Telegram/token.json");
+            var obj = JsonConvert.DeserializeObject<JObject>(content);
+            var token = obj.GetValue("token").ToString();
+
+            var bot = new TelegramBotClient(token);
+            Assert.IsNotNull(bot.BotId);
+        }
+    }
+}
---
+++ Tests/Tfs/Base.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.Services.Client;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace Tests.Tfs
+{
+    public class Base
+    {
+        class TfsSettings
+        {
+            public string Name { get; set; }
+            public string Pass { get; set; }
+            public string Link { get; set; }
+        }        
+        
+        [Test]
+        public void Connect()
+        {
+            var raw = File.ReadAllText("./Tfs/test.json");
+            var obj = JsonConvert.DeserializeObject<TfsSettings>(raw);
+            var uri = new Uri(obj.Link);
+
+            var creds = new VssClientCredentials(new VssBasicCredential(obj.Name, obj.Pass));
+            var connection = new VssConnection(uri, creds);
+            
+            Assert.IsNotNull(connection.AuthenticatedIdentity);
+            Assert.IsTrue(connection.HasAuthenticated);
+        }
+    }
+}
